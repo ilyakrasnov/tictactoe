cb9972ae33626d3631ff8174c3892e39e61a570e
Ilya Krasnov <ilya.krasnov@gmail.com>
2015-03-20 11:19:31 +0100

    First working versioin

--- myApp.js
+++ myApp.js
@@ -1,17 +1,37 @@
 angular.module('ticTacToe', [])
   .controller('TicTacToeCtrl', function($scope) {
     $scope.dimensions = [1, 2, 3]
+    $scope.game = [3,4,5,6,7,8,9,10,11]
+
     $scope.setClicked = function(){
       $scope.class = "clicked";
     }
+
     $scope.moves = 0;
 
-    $scope.updateMoves = function(){
+    $scope.updateMoves = function(position){
       $scope.moves++;
+      $scope.game[position] =  ["X", 0][$scope.moves%2];
+      checkWin();
     }
 
-    $scope.firstPlayer = function(){
-        return $scope.moves % 2 == 1;
+    $scope.checkWin = function(){
+      game = $scope.game
+      return  (game.length != 0) &&
+              (game[0] == game[1] && game[1] == game[2] ||
+               game[3] == game[4] && game[4] == game[5] ||
+               game[6] == game[7] && game[7] == game[8] ||
+               game[0] == game[3] && game[3] == game[6] ||
+               game[1] == game[4] && game[4] == game[7] ||
+               game[2] == game[5] && game[5] == game[8] ||
+               game[0] == game[4] && game[4] == game[8] ||
+               game[2] == game[4] && game[4] == game[6]);
     }
+
+    $scope.currentCell = 0;
+    $scope.updateCurrentCell = function(){
+      $scope.currentCell ++;
+    }
+
 
   });
