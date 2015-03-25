c9e34f82bbffe9055b8b800ebf82e2ced9fe41e6
Ilya Krasnov <ilya.krasnov@gmail.com>
2015-03-25 15:10:43 +0100

    Refactor game array to array of arrays

--- myApp.js
+++ myApp.js
@@ -6,8 +6,8 @@ angular.module('ticTacToe', [])
     $scope.multiplayer = false
 
     $scope.moves = 0;
-    $scope.game = []
-    $scope.emptyCells = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    $scope.game = [[null, null, null], [null, null, null], [null, null, null]]
+    $scope.emptyCells = [[0,1,2],[0,1,2],[0,1,2]]
 
     $scope.winners = [];
     $scope.xScore = 0;
@@ -18,29 +18,30 @@ angular.module('ticTacToe', [])
       return _.last($scope.winners)[0];
     }
 
-    $scope.updateMoves = function(position){
-      console.log($scope.gameStart());
+    $scope.updateMoves = function(i, j){
       if ($scope.moves == 9) return;
-      if ($scope.game[position] != null) return;
+      if ($scope[i] != null && $scope.game[i][j] != null) return;
 
-      updateEmptyCells(position);
+      updateEmptyCells(i, j);
       $scope.moves++;
+      $scope.game[i][j] =  $scope.players[$scope.moves%2];
 
-      $scope.game[position] =  $scope.players[$scope.moves%2];
 
       if ($scope.checkWin() == true) {
 
-        $scope.winners = $scope.winners.concat($scope.game[position].split(""));
+        $scope.winners = $scope.winners.concat($scope.game[i][j].split(""));
 
         updateScore();
         return
       }
 
       if ($scope.multiplayer == false && computersMove() == true) {
-        next_position = _.sample($scope.emptyCells);
-        $scope.updateMoves(next_position);
+        randomRow = randomEmptyRow();
+        randomCol = randomEmptyCol(randomRow);
+        $scope.updateMoves(randomCol, randomRow);
       };
     }
+
 
     $scope.setComputerPlayer = function() {
       $scope.multiplayer = !$scope.multiplayer;
@@ -82,8 +83,23 @@ angular.module('ticTacToe', [])
     }
 
 // Helper functions
-    var updateEmptyCells = function(position){
-      $scope.emptyCells = _.without($scope.emptyCells, position);
+    var randomEmptyRow = function(){
+      if (_.sample($scope.emptyCells).length>0) {
+        return _.indexOf($scope.emptyCells, _.sample($scope.emptyCells));
+      }else {
+        randomEmptyRow();
+      }
+    }
+
+    var randomEmptyCol = function(row){
+      array = $scope.emptyCells[row];
+      return _.indexOf(array, _.sample(array));
+    }
+
+    var updateEmptyCells = function(i, j){
+      $scope.emptyCells[i] = _.without($scope.emptyCells[i], j);
+      // console.log($scope.emptyCells);
+
     }
 
     var compare = function(a, b, c) {
